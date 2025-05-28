57cdfa1b763a4ecec8c35a5592891e9d33756ed2
sberzonskiene <s.berzonskiene@gmail.com>
2025-05-28 14:38:00 +0300

    update

--- js/components/trio-tabs.js
+++ js/components/trio-tabs.js
@@ -2,20 +2,25 @@ export function trioTabs() {
     const HTML = ` 
         <div class="container">
             <div class="row">
-                <div class="col-6">
-                    <div>LOGO</div>
+                <div class="col-sm-1 col-lg-4 col-xl-6 ">
+                    <div>
+                    <img class="logo1" src="./img/logo1.png" alt="logo1"></div>
                     <h2>Creative Design</h2>
                     <p>Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
                 </div>
-        
+
+
                 <div class="col-3">
-                    <div>LOGO</div>
+                
+                    <div>
+                    <img class="logo2" src="./img/logo2.png" alt="logo2"></div>
                     <h2>Easy To Start</h2>
                     <p>Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
                 </div>
            
                 <div class="col-3">
-                    <div>LOGO</div>
+                    <div>
+                    <img class="logo3" src="./img/logo3.png" alt="logo3"></div>
                     <h2>Management</h2>
                     <p>Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
                 </div>
