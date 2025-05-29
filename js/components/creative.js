d017feefce36fa583030fb26f166eb09775f87d4
Aidas415 <aidasambotas@gmail.com>
2025-05-29 13:55:47 +0300

    creative.js papildymai

--- js/components/creative.js
+++ js/components/creative.js
@@ -3,11 +3,13 @@ export function creative() {
     let HTML = `
     <div class="creative">
         <div class="col-12 col-lg-6">
-            <img src="img/020.png" alt="ketveriukė">
+            <img src="#" alt="ketveriukė">
         </div>
         <div class="col-12 col-lg-6">
-            <div></div>
-            <h3>We're A Creative Software Landing Page</h3>
+            <div>
+                <i></i>
+            </div>
+            <h2>We're A Creative Software Landing Page</h3>
             <p>We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
             <ul>
                 <li>Fully Responsive</li>
@@ -15,7 +17,7 @@ export function creative() {
                 <li>Built with Sass</li>
                 <li>SVG Icon</li>
             </ul>
-            <button type="submit"></button>
+            <button class="btn-white" type="submit">learn more</button>
         </div>
     </div>`;
 
