2f1f5c5db6b2421c758ddf49e853cef44196168b
sberzonskiene <s.berzonskiene@gmail.com>
2025-05-30 12:26:24 +0300

    update

--- js/components/trio-tabs.js
+++ js/components/trio-tabs.js
@@ -2,25 +2,25 @@ export function trioTabs() {
     const HTML = ` 
         <div class="container">
             <div class="row">
-                <div class="col-sm-1 col-lg-4 col-xl-6 ">
+                <div class="col-12 col-md-12 col-lg-4 col-xl-6">
                     <div>
-                    <img class="logo1" src="./img/logo1.png" alt="logo1"></div>
+                    <img class="logo1" src="./img/trio-tabs/logo1.png" alt="logo1"></div>
                     <h2>Creative Design</h2>
                     <p>Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
                 </div>
 
 
-                <div class="col-3">
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
                 
                     <div>
-                    <img class="logo2" src="./img/logo2.png" alt="logo2"></div>
+                    <img class="logo2" src="./img/trio-tabs/logo2.png" alt="logo2"></div>
                     <h2>Easy To Start</h2>
                     <p>Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
                 </div>
            
-                <div class="col-3">
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
                     <div>
-                    <img class="logo3" src="./img/logo3.png" alt="logo3"></div>
+                    <img class="logo3" src="./img/trio-tabs/logo3.png" alt="logo3"></div>
                     <h2>Management</h2>
                     <p>Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
                 </div>
