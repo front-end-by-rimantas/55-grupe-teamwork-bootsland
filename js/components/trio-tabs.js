9f49eab8f87d9a97d175d095882ebe5a1bb90846
sberzonskiene <s.berzonskiene@gmail.com>
2025-05-30 15:58:28 +0300

    update

--- js/components/trio-tabs.js
+++ js/components/trio-tabs.js
@@ -1,34 +1,26 @@
-export function trioTabs() {
-    const HTML = ` 
-        <div class="container">
+import { trioData } from "../components/trioData.js";
+
+export function trioTabs() {}
+
+    let HTML = '';
+
+    for (const item of trioData) {
+        HTML += `
+        <div class="container trio-tabs">
             <div class="row">
                 <div class="col-12 col-md-12 col-lg-4 col-xl-6">
-                    <div>
-                    <img class="logo1" src="./img/trio-tabs/logo1.png" alt="logo1"></div>
-                    <h2>Creative Design</h2>
-                    <p>Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
+                    <div class="trio-logo">
+                    <img src="./img/trio-tabs/${item.logo}" alt="${item.imgAlt}"></div>
+                    <h2 class="trio-title">${item.title}</h2>
+                    <p class="trio-desc">${item.desc}</p>
                 </div>
 
-
-                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
                 
-                    <div>
-                    <img class="logo2" src="./img/trio-tabs/logo2.png" alt="logo2"></div>
-                    <h2>Easy To Start</h2>
-                    <p>Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
-                </div>
-           
-                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-                    <div>
-                    <img class="logo3" src="./img/trio-tabs/logo3.png" alt="logo3"></div>
-                    <h2>Management</h2>
-                    <p>Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
-                </div>
             </div>
         </div>`;
-
+    
  document
         .getElementById('app')
         .insertAdjacentHTML('beforeend', HTML);       
 
-}
+};
