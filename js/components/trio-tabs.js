a9ca4ad109ce6a4ddd1566df60022abbecf51831
sberzonskiene <s.berzonskiene@gmail.com>
2025-05-30 20:01:39 +0300

    update

--- js/components/trio-tabs.js
+++ js/components/trio-tabs.js
@@ -1,21 +1,29 @@
-import { trioData } from "../components/trioData.js";
+export function trioTabs() {
 
-export function trioTabs() {}
-
-    let HTML = '';
-
-    for (const item of trioData) {
-        HTML += `
+    const HTML = `
         <div class="container trio-tabs">
             <div class="row">
                 <div class="col-12 col-md-12 col-lg-4 col-xl-6">
                     <div class="trio-logo">
-                    <img src="./img/trio-tabs/${item.logo}" alt="${item.imgAlt}"></div>
-                    <h2 class="trio-title">${item.title}</h2>
-                    <p class="trio-desc">${item.desc}</p>
+                    <img src="./img/trio-tabs/logo1.png" alt="logo1"></div>
+                    <h2 class="trio-title">Creative Design</h2>
+                    <p class="trio-desc">Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
                 </div>
 
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
                 
+                    <div class="trio-logo">
+                    <img class="logo2" src="./img/trio-tabs/logo2.png" alt="logo2"></div>
+                    <h2 class="trio-title">Easy To Start</h2>
+                    <p class="trio-desc">Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
+                </div>
+           
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                    <div class="trio-logo">
+                    <img class="logo3" src="./img/trio-tabs/logo3.png" alt="logo3"></div>
+                    <h2 class="trio-title">Management</h2>
+                    <p class="trio-desc">Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
+                </div>
             </div>
         </div>`;
     
