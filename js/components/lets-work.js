28475f1e6f4a5bb0becef751c20a021362fc7748
kberzonskis <k.berzonskis@gmail.com>
2025-05-30 15:55:34 +0200

    buttons

--- js/components/lets-work.js
+++ js/components/lets-work.js
@@ -6,15 +6,14 @@ export function letsWork() {
         
              <div class = "row">
                                          
-                 <div class="lets-work-text col-6">
+                 <div class="lets-work-text col-6 " >
                 
-                    <h6>Ever created for</h6>
-                    <h1>Let's Work With</h1>
-                    <h1>Bootsland</h1>
-                    <h5>Build a Beautiful, Clean & Modern Design website with flexible Bootstrap components.</h5>
-                    <button class="btn-get-started" id="get-started">Get Started</button>
-                    <button class="btn-learn-more" id="learn-more">Learn more</button>
-                    
+                    <h6 class="badge">Ever Created For</h6>
+                    <h1 class="h1-1">Let's Work With</h1>
+                    <h1 class="h1-2">Bootsland</h1>
+                    <h5 class= "h5-1">Build a Beautiful, Clean & Modern Design website with flexible Bootstrap components.</h5>
+                    <button class="bt btn-learn-more btn-blue" id="learn-more">Learn more</button>
+                    <button class="bt btn-get-started btn-white" id="get-started">Get Started</button>
                     <div class = "media-ico">  
 
                         <a href="#"><img class="media-icon" src="/img/ThMSO.png" alt="Media Icon"></a> 
