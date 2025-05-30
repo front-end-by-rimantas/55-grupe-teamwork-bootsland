f1554587b0fe2ddc335c0215284f2ae7ae551d56
Aidas415 <aidasambotas@gmail.com>
2025-05-30 11:43:51 +0300

    creative.js pradžia

--- js/components/creative.js
+++ js/components/creative.js
@@ -1,4 +1,11 @@
+import { creativeData } from "../data/creativeData.js";
+
 export function creative() {
+    let liHTML = '';
+
+    for (const li of creativeData) {
+        liHTML += `<li><i class="fa fa-check-circle></i>${li}</li>`;
+    }
 
     let HTML = `
     <div class="creative">
@@ -11,12 +18,7 @@ export function creative() {
             </div>
             <h2>We're A Creative Software Landing Page</h2>
             <p>We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-            <ul>
-                <li>Fully Responsive</li>
-                <li>Based On Bootstrap 4</li>
-                <li>Built with Sass</li>
-                <li>SVG Icon</li>
-            </ul>
+            <ul>${liHTML}</ul>
             <button class="btn-white" type="submit">learn more</button>
         </div>
     </div>`;
