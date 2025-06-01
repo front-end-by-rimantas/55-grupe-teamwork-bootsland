cc6decf1c566afaf1225c8400c1a74304c048e74
sberzonskiene <s.berzonskiene@gmail.com>
2025-06-01 20:54:11 +0300

    trio-tabs

--- js/components/trio-tabs.js
+++ js/components/trio-tabs.js
@@ -1,6 +1,21 @@
-export function trioTabs() {
+import { trioData } from "../data/trioData.js";    
 
-    const HTML = `
+export function trioTabs() {
+    let tabsHTML = '';
+
+    for (let i = 0; i < 2; i++) {
+        const trio = trioData[i];
+        tabsHTML += `
+         <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+            <div class="trio-logo">
+                <img src="${trio.img}" alt="${trio.imgAlt}">
+            </div>
+                <h2 class="trio-title">${trio.title}</h2>
+                <p class="trio-desc">${trio.desc}</p>
+        </div>`;
+    }
+
+   const HTML = `
         <div class="container trio-tabs">
             <div class="row">
                 <div class="col-12 col-md-12 col-lg-4 col-xl-6">
@@ -9,24 +24,8 @@ export function trioTabs() {
                     <h2 class="trio-title">Creative Design</h2>
                     <p class="trio-desc">Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
                 </div>
-
-                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-                
-                    <div class="trio-logo">
-                    <img class="logo2" src="./img/trio-tabs/logo2.png" alt="logo2"></div>
-                    <h2 class="trio-title">Easy To Start</h2>
-                    <p class="trio-desc">Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
-                </div>
-           
-                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-                    <div class="trio-logo">
-                    <img class="logo3" src="./img/trio-tabs/logo3.png" alt="logo3"></div>
-                    <h2 class="trio-title">Management</h2>
-                    <p class="trio-desc">Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.</p>
-                </div>
-            </div>
+                ${tabsHTML}
         </div>`;
-    
  document
         .getElementById('app')
         .insertAdjacentHTML('beforeend', HTML);       
