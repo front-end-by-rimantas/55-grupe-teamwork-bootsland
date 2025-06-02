63f2855dff9e6a9868c0f8ee3967934d40db3a7e
Aidas415 <aidasambotas@gmail.com>
2025-06-02 14:29:12 +0300

    ikonų tvarkymas

--- js/components/creative.js
+++ js/components/creative.js
@@ -10,12 +10,10 @@ export function creative() {
     let HTML = `
     <div class="container creative">
         <div class="col-12 col-lg-6">
-            <img src="#" alt="ketveriukė">
+            <img src="./img/creative-img/020.png" alt="ketveriukė">
         </div>
-        <div class="col-12 col-lg-6">
-            <div class="ikon-area">
-                <i class"fa fa-exclamation"></i>
-            </div>
+        <div class="col-12 col-lg-6">    
+            <p class="icon-area fa fa-exclamation"></p>
             <h2 class="title">We're A Creative Software Landing Page</h2>
             <p class="description">We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
             <ul>${liHTML}</ul>
