a02e03f0bd76e44ee5c10460102662d679713a9c
Aidas415 <aidasambotas@gmail.com>
2025-06-02 23:23:35 +0300

    img ir tekstai

--- js/components/creative.js
+++ js/components/creative.js
@@ -4,7 +4,7 @@ export function creative() {
     let liHTML = '';
 
     for (const li of creativeData) {
-        liHTML += `<li><i class="fa fa-check-circle"></i>${li}</li>`;
+        liHTML += `<li><i class="fa fa-regular fa-check"></i>${li}</li>`;
     }
 
     let HTML = `
@@ -12,12 +12,12 @@ export function creative() {
         <div class="col-12 col-lg-6">
             <img src="./img/creative-img/020.png" alt="ketveriukė">
         </div>
-        <div class="col-12 col-lg-6">    
-            <p class="icon-area fa fa-exclamation"></p>
+        <div class="col-12 col-lg-6">          
+            <i class="icon-area fa fa-exclamation"></i>
             <h2 class="title">We're A Creative Software Landing Page</h2>
             <p class="description">We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
             <ul>${liHTML}</ul>
-            <button class="btn-white" type="submit">learn more</button>
+            <button class="btn-white button" type="submit">learn more</button>
 
             
         </div>
