52ad6572a5614e299d7beca7ae29347c8c804704
Aidas415 <aidasambotas@gmail.com>
2025-06-03 12:25:41 +0300

    Creative page

--- js/components/creative.js
+++ js/components/creative.js
@@ -12,8 +12,10 @@ export function creative() {
         <div class="col-12 col-lg-6">
             <img src="./img/creative-img/020.png" alt="ketveriukė">
         </div>
-        <div class="col-12 col-lg-6">          
-            <i class="icon-area fa fa-exclamation"></i>
+        <div class="col-12 col-lg-5 m-xl-1 m-xxl-1">
+            <div class="icon-area">          
+                <i class="fa fa-exclamation"></i>
+            </div>
             <h2 class="title">We're A Creative Software Landing Page</h2>
             <p class="description">We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
             <ul>${liHTML}</ul>
