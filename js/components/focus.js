162dc5a84873a4f9c4228d211882312c15b495c2
sberzonskiene <s.berzonskiene@gmail.com>
2025-06-04 15:01:38 +0300

    focus

--- js/components/focus.js
+++ js/components/focus.js
@@ -1,16 +1,27 @@
-export function blog() {
+export function focus() {
     const HTML =`
         <section class="container focus">
             <div class="row">
-                <div class="col-12 >
-                    <h1 class="title">Bootsland Focus on your success</h1>
-                    <p class="desc">We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                <div class="col-12 col-xxl-6>
+                    <div class="text">
+                        <h1>Bootsland Focus on your success</h1>
+                        <p>We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                    </div>    
+                    <div class="client">
+                        <img src="./img/focus/client.png" alt="client">  
+                        <h2 class="name">Kendra Lawt</h2>
+                        <p>Web Developer</p>
+                    </div>
                 </div>
-            <div class="web">
-                <img src=".img/focus/web.png" alt="web">  
-                    
-    `
+                    <div class="col-12 col-xxl-6 m-xl-6">
+                      <div class="foto">    
+                        <img src="./img/focus/focus.png" alt="focus">
+                    </div>
+                </div>
+            </div>
+        </section> `;
+
     document
         .getElementById('app')
         .insertAdjacentHTML('beforeend', HTML);
-}
+};
