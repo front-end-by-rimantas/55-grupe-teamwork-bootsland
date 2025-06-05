be8d8323d2222da25f9b7b54c3db68712e30efa0
sberzonskiene <s.berzonskiene@gmail.com>
2025-06-05 15:12:50 +0300

    focus section

--- js/components/focus.js
+++ js/components/focus.js
@@ -1,25 +1,28 @@
 export function focus() {
-    const HTML =`
-        <section class="container focus">
+    const HTML = `
+        <div class="container focus">
             <div class="row">
-                <div class="col-12 col-xxl-6>
-                    <div class="focus">
-                        <h1 class="focus-title">Bootsland Focus on your success</h1>
-                        <p class="focus-desc">We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-                    </div>           
-                    <div class="client">
-                        <img src="./img/focus/client.png" alt="client">  
-                        <h2 class="client-name">Kendra Lawt</h2>
-                        <p class="client-desc">Web Developer</p>
-                    </div>
-                </div>
-                    <div class="col-12 col-lg-6 m-xl-6">
+                <div class="col-12 col-lg-6">
+                    <div class="focus-content">
+                        <div class="focus-text">
+                            <h1 class="focus-title">Bootsland Focus on your success</h1>
+                            <p class="focus-desc">We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                        </div>           
+                            <div class="client">
+                                <img src="./img/focus/client.png" alt="client">
+                            <div class="client-text">  
+                                <h2 class="client-name">Kendra Law</h2>
+                                <p class="client-desc">Web Developer</p>
+                            </div>
+                        </div>
+                    </div> 
+                </div>    
+                    <div class="col-12 col-lg-6">
                       <div class="foto">    
-                        <img src="./img/focus/focus.png" alt="focus">
+                        <img src="./img/focus/focus.png" alt="focus"></div>
                     </div>
-                </div>
             </div>
-        </section> `;
+        </div>`;
 
     document
         .getElementById('app')
