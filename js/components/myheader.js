ec183cb1d9a89bfb61693b3eb66afb08d7c11a8e
Aidas415 <aidasambotas@gmail.com>
2025-06-09 15:08:25 +0300

    headerio tvarkymas

--- js/components/myheader.js
+++ js/components/myheader.js
@@ -2,8 +2,8 @@ import { headerMenuData } from "../data/myheaderData.js";
 
 export function header() {
     let HTML = `
-        <div id="app" class="container header header-fixed">
-            <div class="row">
+        <div id="app" class="container header">
+            <div class="row no-margin-bottom">
                 <div class="col-12 header-content">
                     <div class="logo">       
                         <a class="startlogo" href="#">boots<span class="endlogo">Land.</span></a>
@@ -13,26 +13,43 @@ export function header() {
                     <button class="btn-blue" type="submit">buy now</button>
                 </div>
             </div>
-        </div>
-        <div class="mobile-main-header">
-        ${headerMenu(headerMenuData)}
+            <div class="row">               
+                <div class="mobile-main-header col-12">
+                ${headerMenu(headerMenuData)}
+                </div>
+            </div>
         </div>`;
 
     document
         .getElementById('app')
         .insertAdjacentHTML('beforeend', HTML);
-        
-        const hamburgerDOM = document.querySelector('.hamburger');
-        const mobileHeaderDOM = document.querySelector('.mobile-main-header');
-        
+ 
+    const headerDOM = document.querySelector('.header')
+    const hamburgerDOM = document.querySelector('.hamburger');
+    const mobileHeaderDOM = document.querySelector('.mobile-main-header');
+
+    let clickCount = 0;
     hamburgerDOM.addEventListener('click', () => {
-        mobileHeaderDOM.classList.add('show');
+        if (clickCount++ % 2 === 0) {
+            mobileHeaderDOM.classList.add('show');           
+        } else {
+            mobileHeaderDOM.classList.remove('show');
+        }
     })
 
     addEventListener('keydown', event => {
         if (event.key === 'Escape') {
             mobileHeaderDOM.classList.remove('show');
         }
+    })
+
+    addEventListener('scroll', () => {
+        if (document.scrollingElement.scrollTop > 280) {
+            headerDOM.classList.add('header-fixed');
+        } else {
+            headerDOM.classList.remove('header-fixed');
+        }
+        
     })
 }
 
