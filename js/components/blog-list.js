04417085700f868342d2557caf5d03a76d28371f
kberzonskis <k.berzonskis@gmail.com>
2025-06-10 00:10:39 +0200

    00:10

--- js/components/blog-list.js
+++ js/components/blog-list.js
@@ -1,136 +1,47 @@
 import { bloglistData } from "../data/blog-listData.js"; 
 
-export function bloglist() {
-   let h2HTML =  ""
-   for (const h2 of bloglistData) {
-      h2HTML +=`<h2 class="h1-1">${h2}</h2>`}
+export function bloglist() 
+{
+   let blistHTML =  '';
 
-   
-   let HTML = `<section>
-       <div class="row top col-12 col-6">
-
-                <div class="top1 col-12 col-xxl-4">
-                    <h3>From Our Blog List Latest Feed</h2>
-                </div>
-
-                <div class = "top2 col-12  col-xxl-6"> 
-                   <h2>All types of businesses need access to development resources, so we give you the option to decide how much you need to use.</h2></h2>
-                </div> 
-       
-         </div>
-       
-
-      <div class="row ">
-
+   for (let i = 0; i < bloglistData.length; i++) {
+      const blis = bloglistData[i];
+      blistHTML += ` 
                 <div class="card-1 col-12 col-xl-4"> 
-                     
-                    <div class = "img-container">
-                    <div class="img-add">15<br>July </div>
-                    <img class="list-1" src="./img/blog-list/Blog-list-1.3.png">
+                     <div class = "img-container">
+                        <div class='${blis.class}'>15<br>July</div>
+                        <img class="list-1" src="${blis.img}">
                     </div>
-                    <a class="card-1-a">Sass</a>
-                    <h3 class="card-h3">Bootsland Perfect Performance landing Page</h3>
-                    <h4 class="card-h4">Businesses need access to development resources serspiciatis unde omnis iste natus error.</h4>
+                    <a class="card-1-a">${blis.card1}</a>
+                    <h3 class="card-h3">${blis.h3}</h3>
+                    <h4 class="card-h4">${blis.h4}</h4>
                     <a class="card-ul">                        
                         <i class="fa fa-comment-o" aria-hidden="true">131</i>                    
                         <i class="fa fa-eye" aria-hidden="true">25</i>               
                         <i class="fa fa-comment-o" aria-hidden="true">131</i>                                                            
-                     </a>
-                              
+                     </a>    
+                </div>`;
+   }      
+                 
+  
+const HTML = `
+   <section>
+       <div class="row top col-12 col-6">
+                <div class="top1 col-12 col-xxl-4">
+                    <h3>From Our Blog List Latest Feed</h2>
                 </div>
-                
-                <div class= "card-1 col-12 col-xl-4" >
-
-                <div class = "img-container">
-                    <div class="img-add">15<br>July</div>
-                    <img class="list-1" src="./img/blog-list/Blog-list-2.3.png">
-                    </div>
-                     <a class="card-1-a">Marketing</a>
-                     <h3 class="card-h3">The most powerfull template that make you.</h3>
-                     <h4 class="card-h4">Businesses need access to development resources serspiciatis unde omnis iste natus error.</h4>
-                     <a class="card-ul">                        
-                        <i class="fa fa-comment-o" aria-hidden="true">131</i>                    
-                        <i class="fa fa-eye" aria-hidden="true">25</i>               
-                        <i class="fa fa-comment-o" aria-hidden="true">131</i> 
-                </div>
-
-                <div class= "card-1 col-12 col-xl-4">
-
-                      <div class = "img-container">
-                    <div class="img-add">15<br>July </div>
-                    <img class="list-1" src="./img/blog-list/blog-list-3.3.png">
-                    </div>
-                    <a class="card-1-a">Landing</a>
-                    <h3 class="card-h3">A brand for a company is like a reputation person.</h3>
-                    <h4 class="card-h4">Businesses need access to development resources serspiciatis unde omnis iste natus error.</h4>
-                    <a class="card-ul">                        
-                        <i class="fa fa-comment-o" aria-hidden="true">131</i>                    
-                        <i class="fa fa-eye" aria-hidden="true">25</i>               
-                        <i class="fa fa-comment-o" aria-hidden="true">131</i> 
-
-                </div>
-
-              
- 
-
-
+                <div class = "top2 col-12  col-xxl-6"> 
+                   <h2>All types of businesses need access to development resources, so we give you the option to decide how much you need to use.</h2></h2>
+                </div> 
+      </div>
+       
+      <div class="row ">
+         ${blistHTML}
+        
       </div>    
-      
-
-
-
-
-
-
-
-
-
-
-    </section>
- `;  
-
+   </section>  `;
+       
   document
         .getElementById('app')
         .insertAdjacentHTML('beforeend', HTML);
    }
-
-
-
-/*
-   
-   let h2HTML =  ""
-   for (const h2 of bloglistData) {
-      h2HTML +=`<h2 class="h1-1">${h2}</h2>`}
-
-   
-   let HTML = `<div class="row">  
-   
-
-                <div class="column col-12 col-xl-6">
-                <div class="text">
-                    <h6 class="badge"> Ever Created For</h6>
-                    <h2 class="h1-1">${h2HTML}</h2>
-                    <h2 class="h1-2">Bootsland</h2>
-                    <h5 class="h5-1">Build a Beautiful, Clean & Modern Design website with flexible Bootstrap components.</h5>
-               </div>
-
-                <div class="buttons">
-                  <button class="btn-blue" id="learn-more">Learn more</button>
-                  <button class="btn-white" id="get-started">Get Started</button>
-
-                </div> 
-                 </div>
-                                               
-                <div class="img col-12 col-xl-6"> 
-                <img class="img-010" src="./img/lets-work/010.png" alt="Let's Work With Bootsland" > 
-              </div>
-
-
-
-</div> 
- `;  
-
-  document
-        .getElementById('app')
-        .insertAdjacentHTML('beforeend', HTML);
-   } */
--- js/data/blog-listData.js
+++ js/data/blog-listData.js
@@ -1,47 +1,26 @@
-export const bloglistData = [
-   { class: "img-add", 
-    img: "./img/blog-list/Blog-list-2.3.png",
+export const bloglistData = 
+[
+
+{ class: "img-add", 
+    img: "./img/blog-list/Blog-list-1.3.png",
     card1: "Sass",
     h3: "Bootsland Perfect Performance landing Page", 
     h4: "Businesses need access to development resources serspiciatis unde omnis iste natus error.", 
-    ul:                 <i class="fa fa-comment-o" aria-hidden="true">131</i>                    
-                        <i class="fa fa-eye" aria-hidden="true">25</i>               
-                        <i class="fa fa-comment-o" aria-hidden="true">131</i>
+},
+{
+    class: "img-add", 
+    img: "./img/blog-list/Blog-list-2.3.png",
+    card1: "Marketing",
+    h3: "The most powerfull template that make you.", 
+    h4: "Businesses need access to development resources serspiciatis unde omnis iste natus error.", 
+},
 
-
-
-
-
-
-
-   } 
-
-
-
-
-
-
-
-
-    {
-        img: './img/trio-tabs/logo1.png',
-        imgAlt: 'logo1',
-        title: 'Creative Design',
-        desc: 'Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.',
-        class: 'col-12 col-lg-4 col-xl-6',
-    },
-   {
-        img: './img/trio-tabs/logo2.png',
-        imgAlt: 'logo2',
-        title: 'Easy To Start',
-        desc: 'Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.',
-        class: 'col-12 col-md-6 col-lg-4 col-xl-3',
-    },
-    {
-        img: './img/trio-tabs/logo3.png',
-        imgAlt: 'logo3',
-        title:'Management',
-        desc: 'Serspiciatis unde omnis iste natus error sit doloremque laudantium, totam rem aperiam.',
-        class: 'col-12 col-md-6 col-lg-4 col-xl-3',
-    },
+          
+    {class: "img-add", 
+    img: "./img/blog-list/blog-list-3.3.png",
+    card1: "Landing",
+    h3: "A brand for a company is like a reputation person.", 
+    h4: "Businesses need access to development resources serspiciatis unde omnis iste natus error.", 
+}      
+    
 ];
