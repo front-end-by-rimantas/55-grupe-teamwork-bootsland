8b599eb0e1e1da43e00ab6bf04e327225d07a46f
sberzonskiene <s.berzonskiene@gmail.com>
2025-06-10 13:44:04 +0300

    footer

---
+++ js/data/footerData.js
@@ -0,0 +1,29 @@
+export const footerData = [
+   
+    {
+        text:`<div class="about">
+                    <a>About</a>
+                    <a>Shop</a>
+                    <a>Faq</a>
+                    <a>Blogs</a>
+                    <a>Contact Us</a>
+                </div> `,
+    },
+
+    {
+        text: `<div class="about">
+                    <a>Content Writing</a>
+                    <a>Documentation</a>
+                    <a>Account</a>
+                    <a>Careers</a>
+                </div> `,
+    },
+    {
+        text: `<div class="about">
+                    <a>Term Of Service</a>
+                    <a>Privacy Policy</a>
+                    <a>Support</a>
+                </div> `,
+    },
+
+]; 
